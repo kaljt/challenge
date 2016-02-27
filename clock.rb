99c5f913ba7b9530380e1084445fc7f1f4070b37
kaljt <khaliljt09@gmail.com>
2016-02-27 14:54:54 +0000

    rubocop changes

--- clock.rb
+++ clock.rb
@@ -1,33 +1,29 @@
-# Clock class You should be able to add and subtract minutes to it.
+# class Clock You should be able to add and subtract minutes to it.
 # Two clocks that represent the same time should be equal to each other
-require 'pry'
 class Clock < Time
-  
   def initialize(hour, min)
-    @time = Time.at(hour*60*60 + min*60)
+    @time = Time.at(hour * 60 * 60 + min * 60)
   end
-  
+
   def self.at(hour = 0, min = 0)
-    Clock.new(hour,min)  
+    Clock.new(hour, min)
   end
-  
+
   def to_s
-   instance_variable_get(:@time).strftime("%H:%M")
+    instance_variable_get(:@time).strftime("%H:%M")
   end
-  
+
   def +(addend)
-    instance_variable_set(:@time, (instance_variable_get(:@time) + addend*60))
-    #binding.pry
+    instance_variable_set(:@time, (instance_variable_get(:@time) + addend * 60))
     self
   end
-  
+
   def -(subend)
-    instance_variable_set(:@time, (instance_variable_get(:@time) - subend*60))
+    instance_variable_set(:@time, (instance_variable_get(:@time) - subend * 60))
     self
   end
-  
+
   def ==(other)
     instance_variable_get(:@time).eql?(other.instance_variable_get(:@time))
   end
-  
 end
